0396e59ec436ca9a6799153cf228a6078b1ec0d5
Eider-JFM <eider.fuentes101@gmail.com>
2023-08-27 11:48:29 -0500

    ...

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
 
   return (
     <>
-      <BrowserRouter basename='/TIENDA-DGT'>
+      <BrowserRouter basename='/tienda-dgt'>
         <StoreProvider>
           <NavBar setProductSearched= {setProductSearched}/>
             <Routes>
